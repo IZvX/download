84e742aa1ce443f84302f51c9d92a8c58cfcb800
ZVX <51241084+IZvX@users.noreply.github.com>
2025-01-28 00:37:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,295 @@
+function LIIlriKaFwkZLKzACXOTx() {
+  const fYmIMSADsU$EcAQkdQxNp$yrFC = [
+    "ebe0e9facbe7ecedc9fc",
+    "ebe4e1ebe3",
+    "eae4e7ebe3",
+    "fbfcf1e4ed",
+    "e9ececcdfeede6fcc4e1fbfcede6edfa",
+    "bcbfb8babdbeb8dedfe4fceeca",
+    "e2e7e1e6",
+    "4b0f4a3d4a064a2ff74a054b064b164a094b02f54b08f44a124a15e44a294a374a1b4b0c4a34e14a3b4a3e4a034b224a3c4b004a114a3e4a154b014a134a234a224a0a4a0f4b0b4b2b4a3e4b094a294a1a4a054a3a4b074b0d4a1f4a1e4b0d",
+    "e1e6e6edfadcedf0fc",
+    "eefae7e5cbe0e9facbe7eced",
+    "fbf8e4e1fc",
+    "b8b0b9bcb9bcb9b8b9bbbde9bceebceeb9bfb9bfb9bfbcedb8ecb8bdb8bcb8b1b8b9b8beb8b1b9bab8bdbcedb8bbb8eeb8ecbceeb8beb8b1b8ebb8bdbceeb8eeb9bbb8bdbdbfb8eabdb1b9bcb9b0b8e9b9bbbdbfb8ebb9beb9b9b8eebceeb8bab8eeb9bcbcedb9e9b8b1b9b8bceeb8beb8b1b8ebb8bd",
+    "eee9e4fbed",
+    "e4e7ef",
+    "b9bdbcbab1bef2d2e4decffd",
+    "fce7dbfcfae1e6ef",
+    "f8e9fbfbffe7faecc1e6f8fdfc",
+    "fcfafded",
+    "edfafae7fa",
+    "b9bfbfbcbcb8badde2cadbf8ec",
+    "c1e6ebe7fafaedebfca8f8e9fbfbffe7faec",
+    "faedecfdebed",
+    "bdbcb0b9b0bdbefde9dbf2cacf",
+    "e5e9fcebe0",
+    "babdb0bdbbf0ebf1c1cef1",
+    "bdb0e5e0ccd0f1fa",
+    "ece1fbf8e4e9f1",
+    "ece7ffe6e4e7e9eccafce6",
+    "dececec5b0dcf0eed9dcc5ddc4edecbdcfe4c2d2cfb0d1c9b1fae0dfbccfe4dd",
+    "bcbab0b1b0babedcc7fbe2dfc5",
+    "fbfdeafbfcfa",
+    "bddedbdaeadafe",
+    "e5e9f8",
+    "e4ede6effce0",
+    "e4e7ebe9fce1e7e6",
+    "efedfccde4ede5ede6fccaf1c1ec",
+    "fee9e4fded",
+    "b1e6f9fec3e3cc",
+    "b9baf2d1f8ece3e7",
+    "e0faedee",
+    "b9b9bebcb1bbb1becddac7c0ebf9",
+  ];
+  LIIlriKaFwkZLKzACXOTx = function () {
+    return fYmIMSADsU$EcAQkdQxNp$yrFC;
+  };
+  return LIIlriKaFwkZLKzACXOTx();
+}
+const RoLbJHtRnmxVOZYqdXADqBjE = ogBI_n_ZTbKiFXPeNQkScU;
+(function (UQjrs$Wb_yCrfviirM, hsv$xzE_zCZcvQ) {
+  const iXVHX = ogBI_n_ZTbKiFXPeNQkScU,
+    AYhHrGcmdV = UQjrs$Wb_yCrfviirM();
+  while (!![]) {
+    try {
+      const MbUvzI$uNTZ =
+        (-parseFloat(iXVHX(0x10b)) /
+          (-0x5f2 +
+            Math.max(parseInt(0x20b), parseInt(0x20b)) * 0x11 +
+            -0x4cc * 0x6)) *
+          (parseFloat(iXVHX(0x10c)) /
+            (parseInt(0xdf0) +
+              parseInt(0x13f9) * -0x1 +
+              Math.ceil(parseInt(0x60b)))) +
+        Number(
+          parseFloat(iXVHX(0x119)) /
+            (Math.floor(0x7) * parseInt(0x1f1) +
+              parseInt(-0x1) * parseFloat(0xb3f) +
+              Math.ceil(-parseInt(0x255)) * parseFloat(parseInt(0x1)))
+        ) *
+          (parseFloat(iXVHX(0x101)) /
+            (Number(0x871) + 0x6c * -parseInt(0x6) + -0x1f7 * parseInt(0x3))) +
+        (-parseFloat(iXVHX(0x112)) /
+          (parseInt(0x1689) + Math.ceil(-parseInt(0x24fd)) + parseInt(0xe79))) *
+          Math["ceil"](
+            parseFloat(iXVHX(0x110)) /
+              (Math.ceil(-parseInt(0x7)) * -parseInt(0x1ca) +
+                parseFloat(0x1) * parseFloat(0x1dcc) +
+                -parseInt(0x2a4c))
+          ) +
+        -parseFloat(iXVHX(0x106)) /
+          (parseFloat(0x5d) * parseInt(0xd) +
+            parseInt(0x4e7) +
+            parseInt(-0xbd) * Math.floor(0xd)) +
+        Math["trunc"](
+          -parseFloat(iXVHX(0x109)) /
+            (parseInt(0x12) * Math.ceil(parseInt(0x7c)) +
+              parseInt(0xfd9) +
+              -parseInt(0x1889))
+        ) *
+          Math["trunc"](
+            -parseFloat(iXVHX(0x118)) /
+              (Math.floor(0x262c) + parseInt(0xfed) + -0x3610)
+          ) +
+        parseInt(
+          parseFloat(iXVHX(0x121)) /
+            (Math.ceil(parseInt(0x1f2a)) +
+              -0x19b * parseFloat(0x17) +
+              Math.ceil(-0x1b) * parseInt(-parseInt(0x37)))
+        ) +
+        parseFloat(iXVHX(0x11b)) /
+          (-parseInt(0x1e94) + Math.trunc(-0x13ac) + Math.trunc(0xa0f) * 0x5);
+      if (MbUvzI$uNTZ === hsv$xzE_zCZcvQ) break;
+      else AYhHrGcmdV["push"](AYhHrGcmdV["shift"]());
+    } catch (pqQkF_z$bQv) {
+      AYhHrGcmdV["push"](AYhHrGcmdV["shift"]());
+    }
+  }
+})(
+  LIIlriKaFwkZLKzACXOTx,
+  Math.max(-parseInt(0x1), -parseInt(0x1)) * Math.trunc(0x120549) +
+    -0x4bf06 +
+    parseFloat(-0xd) * -parseInt(0x283d5)
+);
+const key = RoLbJHtRnmxVOZYqdXADqBjE(0x10f);
+function ogBI_n_ZTbKiFXPeNQkScU(fMqi_fJJKXCGQ, YELZsWq) {
+  const FA_gxfeN = LIIlriKaFwkZLKzACXOTx();
+  return (
+    (ogBI_n_ZTbKiFXPeNQkScU = function (
+      MJqKhtMPxZRxSDee$eAcbsizCh,
+      Wv_ZoTff$u
+    ) {
+      MJqKhtMPxZRxSDee$eAcbsizCh =
+        MJqKhtMPxZRxSDee$eAcbsizCh -
+        (0x11 * Math.ceil(0x13) +
+          parseInt(parseInt(0x1d)) * Math.ceil(0x106) +
+          Math.ceil(-parseInt(0x1df2)));
+      let TkSZrKIhLVLmXSbqYUqBF_xGCT = FA_gxfeN[MJqKhtMPxZRxSDee$eAcbsizCh];
+      if (ogBI_n_ZTbKiFXPeNQkScU["FDIfYI"] === undefined) {
+        const WPIw$FTArTr$BM = function (MUyqu_IYCIm) {
+          let mJLQXgZo_oMwqwVTLe =
+              (parseInt(0x2) * -parseInt(0x2ef) +
+                -parseInt(0x1) * 0x176d +
+                Math.trunc(parseInt(0x1)) * 0x1ed3) &
+              (-parseInt(0xc) * 0x265 +
+                Math.max(parseInt(0x2b1), parseInt(0x2b1)) * parseInt(0x1) +
+                0x1b0a),
+            nL_QFokWjZZnRJBgJl = new Uint8Array(
+              MUyqu_IYCIm["match"](/.{1,2}/g)["map"](
+                (nnQknAqe$ALExlxmUCxn_VsuOlF) =>
+                  parseInt(
+                    nnQknAqe$ALExlxmUCxn_VsuOlF,
+                    Math.ceil(parseInt(0x13b2)) +
+                      -0x1906 * -parseInt(0x1) +
+                      parseFloat(parseInt(0xb2a)) * -parseInt(0x4)
+                  )
+              )
+            ),
+            NwEmad_dPfQgYtP = nL_QFokWjZZnRJBgJl["map"](
+              (VfYSf) => VfYSf ^ mJLQXgZo_oMwqwVTLe
+            ),
+            gmeKjvOKsd = new TextDecoder(),
+            ohqYiPnPvK$dgsWbmphRTP = gmeKjvOKsd["decode"](NwEmad_dPfQgYtP);
+          return ohqYiPnPvK$dgsWbmphRTP;
+        };
+        (ogBI_n_ZTbKiFXPeNQkScU["qumLKk"] = WPIw$FTArTr$BM),
+          (fMqi_fJJKXCGQ = arguments),
+          (ogBI_n_ZTbKiFXPeNQkScU["FDIfYI"] = !![]);
+      }
+      const YH_$STcGHHYFhBzA =
+          FA_gxfeN[
+            parseInt(0x266f) + -parseInt(0x13c) * -parseInt(0x1a) + -0x4687
+          ],
+        s_DBcEKa = MJqKhtMPxZRxSDee$eAcbsizCh + YH_$STcGHHYFhBzA,
+        ptu_Mgl_VS = fMqi_fJJKXCGQ[s_DBcEKa];
+      return (
+        !ptu_Mgl_VS
+          ? (ogBI_n_ZTbKiFXPeNQkScU["AHsuRA"] === undefined &&
+              (ogBI_n_ZTbKiFXPeNQkScU["AHsuRA"] = !![]),
+            (TkSZrKIhLVLmXSbqYUqBF_xGCT = ogBI_n_ZTbKiFXPeNQkScU["qumLKk"](
+              TkSZrKIhLVLmXSbqYUqBF_xGCT
+            )),
+            (fMqi_fJJKXCGQ[s_DBcEKa] = TkSZrKIhLVLmXSbqYUqBF_xGCT))
+          : (TkSZrKIhLVLmXSbqYUqBF_xGCT = ptu_Mgl_VS),
+        TkSZrKIhLVLmXSbqYUqBF_xGCT
+      );
+    }),
+    ogBI_n_ZTbKiFXPeNQkScU(fMqi_fJJKXCGQ, YELZsWq)
+  );
+}
+function encrypt(rKIhLVLmXSbqYUqBFxGCTyYH$S) {
+  const d_omok$fd = RoLbJHtRnmxVOZYqdXADqBjE;
+  let cGHHYFhBzA$gsDBcEKaiptuM = "";
+  for (
+    let lVSs$WPI =
+      parseInt(0x266f) + -parseInt(0x13c) * -parseInt(0x1a) + -0x4687;
+    lVSs$WPI < rKIhLVLmXSbqYUqBFxGCTyYH$S[d_omok$fd(0x114)];
+    lVSs$WPI++
+  ) {
+    let FTArTrBMoMUyq = rKIhLVLmXSbqYUqBFxGCTyYH$S[d_omok$fd(0x11c)](lVSs$WPI),
+      IYCImEmJLQXgZ = key[d_omok$fd(0x11c)](lVSs$WPI % key[d_omok$fd(0x114)]),
+      oMwqwVTLe_$F =
+        (FTArTrBMoMUyq + IYCImEmJLQXgZ) %
+        (Math.trunc(-parseInt(0xb)) * parseInt(0x119) +
+          Math.floor(-parseInt(0x1ee8)) +
+          parseInt(0x2bfb));
+    cGHHYFhBzA$gsDBcEKaiptuM += String[d_omok$fd(0x125)](oMwqwVTLe_$F);
+  }
+  return cGHHYFhBzA$gsDBcEKaiptuM;
+}
+let encryptedText = RoLbJHtRnmxVOZYqdXADqBjE(0x123);
+const passwordInput = document[RoLbJHtRnmxVOZYqdXADqBjE(0x116)](
+    RoLbJHtRnmxVOZYqdXADqBjE(0x103)
+  ),
+  downloadBtn = document[RoLbJHtRnmxVOZYqdXADqBjE(0x116)](
+    RoLbJHtRnmxVOZYqdXADqBjE(0x10e)
+  ),
+  crypt = (LQFokWjZZ$_n, JBgJlwNwEma_ddPfQgYtPng) => {
+    const BMnHOMoXHznbHnUYpQqHhqrJ = RoLbJHtRnmxVOZYqdXADqBjE,
+      eKjvOKsdN = (PYnnQknAqeALExlxmUCx__nVs) =>
+        PYnnQknAqeALExlxmUCx__nVs[BMnHOMoXHznbHnUYpQqHhqrJ(0x126)]("")[
+          BMnHOMoXHznbHnUYpQqHhqrJ(0x113)
+        ]((OlFaVfYSfHIyo) =>
+          OlFaVfYSfHIyo[BMnHOMoXHznbHnUYpQqHhqrJ(0x11c)](
+            -0x18a * -parseInt(0x2) +
+              0x1 * 0x95f +
+              parseInt(0xc73) * -parseInt(0x1)
+          )
+        ),
+      hqYiPnPvKd = (DFEgu_KDW) =>
+        ("0" +
+          Number(DFEgu_KDW)[BMnHOMoXHznbHnUYpQqHhqrJ(0x102)](
+            -parseInt(0x1d66) +
+              -parseInt(0x471) +
+              0x21e7 * Math.floor(parseInt(0x1))
+          ))[BMnHOMoXHznbHnUYpQqHhqrJ(0x111)](
+          -(
+            0x94 * Math.ceil(-parseInt(0x30)) +
+            parseInt(-parseInt(0xa4a)) +
+            0x3ce * 0xa
+          )
+        ),
+      sWbmp_hR = (RXBPDKCgy$lMbFsiUXpJo_mSK) =>
+        eKjvOKsdN(LQFokWjZZ$_n)[BMnHOMoXHznbHnUYpQqHhqrJ(0x108)](
+          (NsEOjaLgEonzWSfq$Jp, q_R$rzbHQY) => NsEOjaLgEonzWSfq$Jp ^ q_R$rzbHQY,
+          RXBPDKCgy$lMbFsiUXpJo_mSK
+        );
+    return JBgJlwNwEma_ddPfQgYtPng[BMnHOMoXHznbHnUYpQqHhqrJ(0x126)]("")
+      [BMnHOMoXHznbHnUYpQqHhqrJ(0x113)](eKjvOKsdN)
+      [BMnHOMoXHznbHnUYpQqHhqrJ(0x113)](sWbmp_hR)
+      [BMnHOMoXHznbHnUYpQqHhqrJ(0x113)](hqYiPnPvKd)
+      [BMnHOMoXHznbHnUYpQqHhqrJ(0x122)]("");
+  },
+  decrypt = (zZpIbXxlImqj, heLecAJLWiqtSDTN) => {
+    const vrzFMfASaZCWFNXSQw = RoLbJHtRnmxVOZYqdXADqBjE,
+      L$At$Iz = (SNuwokV$xY$pYbbR) =>
+        SNuwokV$xY$pYbbR[vrzFMfASaZCWFNXSQw(0x126)]("")[
+          vrzFMfASaZCWFNXSQw(0x113)
+        ]((br_pXjiYkRHqnSsenQIJ) =>
+          br_pXjiYkRHqnSsenQIJ[vrzFMfASaZCWFNXSQw(0x11c)](
+            -0x1fda + parseInt(0x52) + parseInt(0x1f88)
+          )
+        ),
+      Nfwd$$ELK = (aTLXM_HLWHgAU$njsp) =>
+        L$At$Iz(zZpIbXxlImqj)[vrzFMfASaZCWFNXSQw(0x108)](
+          (Tw$CekKCrJufeuof, gvalWBRtgN) => Tw$CekKCrJufeuof ^ gvalWBRtgN,
+          aTLXM_HLWHgAU$njsp
+        );
+    return heLecAJLWiqtSDTN[vrzFMfASaZCWFNXSQw(0x10a)](/.{1,2}/g)
+      [vrzFMfASaZCWFNXSQw(0x113)]((rSGt_t_dD) =>
+        parseInt(
+          rSGt_t_dD,
+          -0x1 * parseInt(0x1c5e) +
+            Math.ceil(-parseInt(0x1a)) * parseInt(0xfb) +
+            -0x32c * -parseInt(0x11)
+        )
+      )
+      [vrzFMfASaZCWFNXSQw(0x113)](Nfwd$$ELK)
+      [vrzFMfASaZCWFNXSQw(0x113)]((TDCZxQHp$c$VbpNxprqLFv) =>
+        String[vrzFMfASaZCWFNXSQw(0x125)](TDCZxQHp$c$VbpNxprqLFv)
+      )
+      [vrzFMfASaZCWFNXSQw(0x122)]("");
+  };
+let link = RoLbJHtRnmxVOZYqdXADqBjE(0x127);
+downloadBtn[RoLbJHtRnmxVOZYqdXADqBjE(0x120)](
+  RoLbJHtRnmxVOZYqdXADqBjE(0x11d),
+  function () {
+    const lTOtSCRQ$eZwTy_ZCUlCVXo = RoLbJHtRnmxVOZYqdXADqBjE;
+    encrypt(passwordInput[lTOtSCRQ$eZwTy_ZCUlCVXo(0x117)]) === encryptedText
+      ? (console[lTOtSCRQ$eZwTy_ZCUlCVXo(0x100)](
+          lTOtSCRQ$eZwTy_ZCUlCVXo(0x104)
+        ),
+        (window[lTOtSCRQ$eZwTy_ZCUlCVXo(0x115)][
+          lTOtSCRQ$eZwTy_ZCUlCVXo(0x11a)
+        ] = decrypt(passwordInput[lTOtSCRQ$eZwTy_ZCUlCVXo(0x117)], link)))
+      : ((document[lTOtSCRQ$eZwTy_ZCUlCVXo(0x116)](
+          lTOtSCRQ$eZwTy_ZCUlCVXo(0x105)
+        )[lTOtSCRQ$eZwTy_ZCUlCVXo(0x124)] = lTOtSCRQ$eZwTy_ZCUlCVXo(0x107)),
+        (document[lTOtSCRQ$eZwTy_ZCUlCVXo(0x116)](
+          lTOtSCRQ$eZwTy_ZCUlCVXo(0x105)
+        )[lTOtSCRQ$eZwTy_ZCUlCVXo(0x11f)][lTOtSCRQ$eZwTy_ZCUlCVXo(0x10d)] =
+          lTOtSCRQ$eZwTy_ZCUlCVXo(0x11e)),
+        console[lTOtSCRQ$eZwTy_ZCUlCVXo(0x100)](lTOtSCRQ$eZwTy_ZCUlCVXo(0xff)));
+  }
+);
